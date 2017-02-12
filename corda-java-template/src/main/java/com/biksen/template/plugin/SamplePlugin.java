4a60b2f7e9ec272221f929ca2fa2708d33499db2
biksen <sen.bikash@gmail.com>
2017-02-12 14:26:33 +0530

    Updated

--- corda-java-template/src/main/java/com/biksen/template/plugin/SamplePlugin.java
+++ corda-java-template/src/main/java/com/biksen/template/plugin/SamplePlugin.java
@@ -1,5 +1,52 @@
 package com.biksen.template.plugin;
 
-public class SamplePlugin {
+import com.esotericsoftware.kryo.Kryo;
+import com.biksen.template.api.SampleApi;
+import com.biksen.template.flow.SampleFlow;
+import com.biksen.template.service.SampleService;
+import net.corda.core.messaging.CordaRPCOps;
+import net.corda.core.node.CordaPluginRegistry;
+import net.corda.core.node.PluginServiceHub;
 
+import java.util.*;
+import java.util.function.Function;
+
+public class SamplePlugin extends CordaPluginRegistry {
+    /**
+     * A list of classes that expose web APIs.
+     */
+    private final List<Function<CordaRPCOps, ?>> webApis = Collections.singletonList(SampleApi::new);
+
+    /**
+     * A list of flows required for this CorDapp.
+     */
+    private final Map<String, Set<String>> requiredFlows = Collections.singletonMap(
+            SampleFlow.Initiator.class.getName(),
+            new HashSet<>(Collections.emptyList()));
+
+    /**
+     * A list of long-lived services to be hosted within the node.
+     */
+    private final List<Function<PluginServiceHub, ?>> servicePlugins = Collections.singletonList(SampleService::new);
+
+    /**
+     * A list of directories in the resources directory that will be served by Jetty under /web.
+     * The template's web frontend is accessible at /web/template.
+     */
+    private final Map<String, String> staticServeDirs = Collections.singletonMap(
+            // This will serve the templateWeb directory in resources to /web/template
+            "template", getClass().getClassLoader().getResource("templateWeb").toExternalForm()
+    );
+
+    /**
+     * Registering the required types with Kryo, Corda's serialisation framework.
+     */
+    @Override public boolean registerRPCKryoTypes(Kryo kryo) {
+        return true;
+    }
+
+    @Override public List<Function<CordaRPCOps, ?>> getWebApis() { return webApis; }
+    @Override public Map<String, Set<String>> getRequiredFlows() { return requiredFlows; }
+    @Override public List<Function<PluginServiceHub, ?>> getServicePlugins() { return servicePlugins; }
+    @Override public Map<String, String> getStaticServeDirs() { return staticServeDirs; }
 }
--- corda-java-template/src/main/java/com/biksen/template/state/SampleState.java
+++ corda-java-template/src/main/java/com/biksen/template/state/SampleState.java
@@ -1,5 +1,22 @@
 package com.biksen.template.state;
 
-public class SampleState {
+import com.biksen.template.contract.SampleContract;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.crypto.CompositeKey;
 
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Define your state object here.
+ */
+public class SampleState implements ContractState {
+    private final SampleContract contract;
+
+    public SampleState(SampleContract contract) { this.contract = contract; }
+
+    @Override public SampleContract getContract() { return contract; }
+
+    /** The public keys of the involved parties. */
+    @Override public List<CompositeKey> getParticipants() { return Collections.emptyList(); }
 }
